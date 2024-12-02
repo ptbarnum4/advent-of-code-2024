8214cf83e2ff1f35121c9abba47e7cf088ba72e7
Peter Barnum <peterbarnum@gmail.com>
2024-12-02 12:09:08 -0700

    cleanup day 1 code

--- day01/index.js
+++ day01/index.js
@@ -5,13 +5,19 @@ const path = require('path');
 const getParsedData = async (file = 'data.txt') => {
   const data = await fs.readFile(path.resolve(__dirname, file), 'utf8');
   return data.split(/\r?\n/).map(s => {
-    const [left, right] = s.split('   ').map(v => parseInt(v.trim()));
+    const [left, right] = s
+      .split(' ')
+      .filter(v => !!v)
+      .map(v => parseInt(v.trim()));
     return { left, right };
   });
 };
 
-/** @param { { left: number; right: number }[]} groups  */
-const sortedGroups = groups => {
+/**
+ * @param { { left: number; right: number }[]} groups
+ * @returns {{ leftGroup: number[], rightGroup: number[] }}
+ */
+const sortGroups = groups => {
   const leftGroup = groups.map(({ left }) => left).sort((a, b) => a - b);
   const rightGroup = groups.map(({ right }) => right).sort((a, b) => a - b);
   return { leftGroup, rightGroup };
@@ -21,33 +27,30 @@ const calculateDistances = ({ leftGroup, rightGroup }) => {
   return leftGroup.map((v1, i) => Math.abs(v1 - rightGroup[i]));
 };
 
+/**
+ * @param {number[]} arr
+ * @returns {number} - Sum of array
+ */
 const add = arr => arr.reduce((total, val) => total + val, 0);
 
 /** @param { { left: number; right: number }[]} groups  */
 const findSimilarityScore = groups => {
-  return groups.map(({ left }) => {
-    let max = 0;
-    groups.forEach(({ right }) => {
-      if (left === right) {
-        max = max + 1;
-        return;
-      }
-    });
-
-    return left * max;
-  });
+  return groups.map(
+    ({ left }) =>
+      groups.reduce((max, { right }) => (left === right ? max + 1 : max), 0) *
+      left
+  );
 };
 
 const day1 = async () => {
   const lines = await getParsedData();
-  const part1Total = add(calculateDistances(sortedGroups(lines)));
+  const part1Total = add(calculateDistances(sortGroups(lines)));
   console.log(`Part 1: ${part1Total}`);
+  // Part 1: 1882714
 
   const part2Total = add(findSimilarityScore(lines));
   console.log(`Part 2: ${part2Total}`);
+  // Part 2: 19437052
 };
-
-// Part 1: 1882714
-// Part 2: 19437052
 
 module.exports = day1;
