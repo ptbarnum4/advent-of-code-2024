0de3ec8453b074c5849a426be670ce94ac76e870
Peter Barnum <peterbarnum@gmail.com>
2024-12-07 14:37:56 -0700

    add partial badges

--- assets/generateBadges.js
+++ assets/generateBadges.js
@@ -1,26 +1,41 @@
+const fsSync = require('fs');
 const fs = require('fs/promises');
 const path = require('path');
 
-const createSVG = (n, pass) => {
-  const color = pass ? '#32FF7D' : '#FF5A46';
-  return `
-  <svg viewBox="0 0 125 50" xmlns="http://www.w3.org/2000/svg" fill="${color}">
-    <text font-family="'Trebuchet MS', sans-serif" font-weight="700" y="45" x="10" font-size="32px">Day ${n}</text>
-  </svg>
-  `;
+const colors = {
+  complete: '#32FF7D',
+  incomplete: '#FF5A46',
+  partial: '#FFE164'
+};
+
+const dirPath = path.resolve(__dirname, 'badges');
+
+const statuses = /**@type {const} */ (['complete', 'incomplete', 'partial']);
+
+/**
+ * @param {number} n
+ * @returns
+ */
+const createSVG = n => {
+  /**
+   * @param {'complete'|'incomplete'|'partial'} status
+   * @returns {Promise<void>}
+   */
+  return async status => {
+    const color = colors[status];
+    const svg = `<svg viewBox="0 0 125 50" xmlns="http://www.w3.org/2000/svg" fill="${color}">\n  <text font-family="'Trebuchet MS', sans-serif" font-weight="700" y="45" x="10" font-size="32px">\n    Day ${n}\n  </text>\n</svg>`;
+
+    const statusDirPath = path.resolve(dirPath, status);
+    if (!fsSync.existsSync(statusDirPath)) {
+      await fs.mkdir(statusDirPath);
+    }
+    await fs.writeFile(path.resolve(dirPath, status, `day${n}.svg`), svg);
+  };
 };
 
 const main = async () => {
   for (let i = 1; i <= 25; i++) {
-    const filePath = path.resolve(__dirname, 'badges');
-    await fs.writeFile(
-      path.resolve(filePath, 'complete', `day${i}.svg`),
-      createSVG(i, true)
-    );
-    await fs.writeFile(
-      path.resolve(filePath, 'incomplete', `day${i}.svg`),
-      createSVG(i, false)
-    );
+    await Promise.all(statuses.map(createSVG(i)));
   }
 };
 
